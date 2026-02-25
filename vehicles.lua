6a1b7bb9243b56f7ee4337fa968b6c90b94e3086
Joshua-Hypersonic <joshuaeger@hypersoniclabs.co>
2026-02-25 12:51:30 -0600

    Auto update vehicles.lua

---
+++ vehicles.lua
@@ -0,0 +1,45 @@
+-- Auto-generated from Google Sheets. Do not edit by hand.
+return {
+  ["bp_bent"] = {
+    brand = "Bentley",
+    label = "Bentley Continental GT",
+    rarity = "common",
+    asset_name = "/HelixVehicles/Blueprints/Cars/Bent/BP_Bent.BP_Bent_C",
+    price = 250000,
+  },
+  ["bp_bugat"] = {
+    brand = "Bugatti",
+    label = "Bugatti Veyron",
+    rarity = "common",
+    asset_name = "/HelixVehicles/Blueprints/Cars/Bugat/BP_Bugat.BP_Bugat_C",
+    price = 350000,
+  },
+  ["bp_cama"] = {
+    brand = "Chevrolet",
+    label = "Camaro ZL1",
+    rarity = "common",
+    asset_name = "/HelixVehicles/Blueprints/Cars/Cama/BP_Cama.BP_Cama_C",
+    price = 60000,
+  },
+  ["bp_chev_zr"] = {
+    brand = "Chevrolet",
+    label = "Chevrolet Silverado ZR1",
+    rarity = "common",
+    asset_name = "/HelixVehicles/Blueprints/Cars/Chev_ZR/BP_Chev_ZR.BP_Chev_ZR_C",
+    price = 120000,
+  },
+  ["bp_h1"] = {
+    brand = "Hummer",
+    label = "H1",
+    rarity = "common",
+    asset_name = "/HelixVehicles/Blueprints/Cars/H1/BP_H1.BP_H1_C",
+    price = 80000,
+  },
+  ["bp_merc"] = {
+    brand = "Mercedes",
+    label = "Mercedes 500E",
+    rarity = "common",
+    asset_name = "/HelixVehicles/Blueprints/Cars/DefaultCar/BP_VehicleCar.BP_VehicleCar_C",
+    price = 45000,
+  },
+}
